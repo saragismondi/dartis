98c3be2cf98f964d22b41b33d55f4479d05c39bc
unknown <saritagismondi@gmail.com>
2024-04-13 18:08:26 -0300

    spinner component modifications

--- docs/table-resize/table.resize.js
+++ docs/table-resize/table.resize.js
@@ -1,32 +1,37 @@
-$(function() {
-    var pressed = false;
-    var start = undefined;
-    var startX, startWidth;
+// $(function() {
+//     var pressed = false;
+//     var start = undefined;
+//     var startX, startWidth;
     
-    $("table th, table td").mousedown(function(e) {
-        start = $(this);
-        pressed = true;
-        startX = e.pageX;
-        startWidth = $(this).width();
-        $(start).addClass("resizing");
-    });
+//     $("table th, table td").mousedown(function(e) {
+//         start = $(this);
+//         pressed = true;
+//         startX = e.pageX;
+//         startWidth = $(this).width();
+//         $(start).addClass("resizing");
+//     });
     
-    $(document).mousemove(function(e) {
-        if(pressed) {
-            var widthChange = e.pageX - startX;
-            $(start).width(startWidth + widthChange);
-            if ($(start).is("th")) {
-                $(start).next("td").width(startWidth + widthChange);
-            } else {
-                $(start).prev("th").width(startWidth + widthChange);
-            }
-        }
-    });
+//     $(document).mousemove(function(e) {
+//         if(pressed) {
+//             var widthChange = e.pageX - startX;
+//             $(start).width(startWidth + widthChange);
+//             if ($(start).is("th")) {
+//                 $(start).next("td").width(startWidth + widthChange);
+//             } else {
+//                 $(start).prev("th").width(startWidth + widthChange);
+//             }
+//         }
+//     });
     
-    $(document).mouseup(function() {
-        if(pressed) {
-            $("table th, table td").removeClass("resizing");
-            pressed = false;
-        }
-    });
-});
+//     $(document).mouseup(function() {
+//         if(pressed) {
+//             $("table th, table td").removeClass("resizing");
+//             pressed = false;
+//         }
+//     });
+// });
+$(document).ready(function(){
+    $(function(){
+      $("table").colResizable();
+    });   
+  });
